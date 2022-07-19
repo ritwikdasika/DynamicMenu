0a59d264fe8ff9cabc0a5b1c902d210e364cad3d
Ritwik Dasika <63977226+ritwikdasika@users.noreply.github.com>
2022-07-19 16:26:46 -0400

    Add files via upload

---
+++ CuberEats with Places API/script.js
@@ -0,0 +1,38 @@
+const menuBars = document.getElementById("menu-bars");
+const overlay = document.getElementById("overlay");
+const nav1 = document.getElementById("nav-1");
+const nav2 = document.getElementById("nav-2");
+const nav3 = document.getElementById("nav-3");
+const nav4 = document.getElementById("nav-4");
+const nav5 = document.getElementById("nav-5");
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+//navigation animation
+function navAnimation(direction1, direction2){
+    navItems.forEach((nav, i) => {
+        nav.classList.replace(`slide-${direction1}-${i + 1}`, `slide-${direction2}-${i + 1}`)
+    });
+}
+
+function toggleNav(){
+    //Toggle menu bars
+    menuBars.classList.toggle('change');
+    overlay.classList.toggle('overlay-active');
+    if(overlay.classList.contains('overlay-active')){
+        //animate in overlay
+        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
+        //animate in nav items
+        navAnimation('out', 'in');
+    } else {
+        //animate out overlay
+        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
+        //animate out nav items
+        navAnimation('in', 'out');
+    }
+}
+
+//event listener
+menuBars.addEventListener('click', toggleNav);
+navItems.forEach((nav) => {
+    nav.addEventListener('click', toggleNav);
+});
